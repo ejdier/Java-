5b3384ede80dfe35f217875407cd129e11c17c2f
ejdier <s13785@poczta.pjwstk.edu.pl>
2017-01-21 13:03:06 +0100

    push

--- src/test/java/AdiProject/AdiProject/AppTest.java
+++ src/test/java/AdiProject/AdiProject/AppTest.java
@@ -1,5 +1,15 @@
 package AdiProject.AdiProject;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import dao.RepositoryCatalog;
+import dao.repository.IRepositoryCatalog;
+import dao.uow.IUnitOfWork;
+import dao.uow.UnitOfWork;
+import domain.EnumerationValue;
+import domain.User;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -10,29 +20,32 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
-}
+	public static void main() {
+	     
+		
+    	try{
+    
+    
+     	
+    	Connection connection = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/workdb");
+		IUnitOfWork uow = new UnitOfWork(connection);
+		IRepositoryCatalog catalogOf = new RepositoryCatalog(connection,uow);
+		User user = new User();
+	     user.setLogin("adrian");
+	     user.setPassword("elejdiero");
+	     
+	     EnumerationValue enumeration = new EnumerationValue();
+	     enumeration.setIntKey(1);
+	     enumeration.setStringKey("ppp");
+	     enumeration.setValue(213);
+	     enumeration.setEnumerationName("changed");
+		catalogOf.users().save(user);
+		catalogOf.enumeration().save(enumeration);
+		
+		catalogOf.saveAndClose();
+    	}catch(SQLException e){
+    		
+    		e.printStackTrace();
+    	}
+    	
+    	}}
